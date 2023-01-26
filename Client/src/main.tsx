f99360f881066a97292af3e31ad5b109bc01135d
ayham <ayhamgm81@gmail.com>
2023-01-26 12:56:47 +0200

    custom theme

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -7,9 +7,22 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import { createTheme, ThemeProvider } from '@mui/material';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      // light: will be calculated from palette.primary.main,
+      main: '#233044',
+      // dark: will be calculated from palette.primary.main,
+      // contrastText: will be calculated to contrast with palette.primary.main
+    },
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ThemeProvider theme={theme}>
+      <App />
+  </ThemeProvider>,
+
 )
--- Client/src/pages/admin/Login.tsx
+++ Client/src/pages/admin/Login.tsx
@@ -1,5 +1,6 @@
 import { Box, Container, Paper, TextField, Typography, useTheme,Checkbox,FormControlLabel, Button} from '@mui/material'
 import Logo from "../../assets/img/Savant.svg"
+import "../../styles/login.css"
 import React from 'react'
 
 export default function AdminLogin() {
@@ -7,24 +8,24 @@ export default function AdminLogin() {
   
   
   return (
-    <Box sx={{ height: "100%", display: "flex" , justifyContent: "center", alignItems: "center" ,bgcolor:"#f3f3f3"}}>
+    <Box className="outerContainer" sx={{ userSelect:"none", height: "100%", display: "flex" , justifyContent: "center", alignItems: "center" ,bgcolor:"#f3f3f3"}}>
       <Container>
         <Box sx={{ display:"flex", justifyContent: "center", alignItems:"center" , gap: theme.spacing(6), flexDirection:"column", width: "100%" }}>
           <img src={Logo} alt="Logo" />
-            <Paper elevation={2} sx={{ padding: theme.spacing(4), maxWidth: "620px", width: "100%"   }}>
+            <Box className='FancyBoxShadow' sx={{ padding: theme.spacing(4), maxWidth: "620px", width: "100%", bgcolor:'white'  }}>
               <Typography variant="h5" component="h1">Admin Sign In</Typography>
                 <form action="" style={{ width:"100%" }}>
-                  <Box sx={{ marginTop: theme.spacing(4), display:"flex", justifyContent: "center", alignItems:"center" , gap: theme.spacing(2), flexDirection:"column" }}>
+                  <Box sx={{ marginTop: theme.spacing(4), display:"flex", justifyContent: "center", alignItems:"center" , gap: theme.spacing(3), flexDirection:"column" }}>
                       <TextField type={"email"} required label="Email" variant="outlined" fullWidth />
                       <TextField type={"password"} required label="Password" variant="outlined" fullWidth />
                       <Box sx={{ width: "100%" }}>
                         <FormControlLabel control={<Checkbox />} label="Remember Me" />
                       </Box>
-                      <Button variant='contained' size='large' type='submit' fullWidth >Hello</Button>
+                      <Button variant='contained' size='large' type='submit'  fullWidth >SIGN in</Button>
 
                   </Box>
                 </form>
-            </Paper>
+            </Box>
         </Box>
       </Container>
 
