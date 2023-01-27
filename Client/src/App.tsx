44b32a6b1239a5bf08755110776d7cccb5702a7c
ayham <ayhamgm81@gmail.com>
2023-01-27 11:02:11 +0200

    made the first ui

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,24 +1,35 @@
-import { Route } from "react-router";
+import { Navigate, Route } from "react-router";
 import { Routes,BrowserRouter,Outlet } from "react-router-dom";
 import RouteGuard from "./components/RouteGuard";
 import Dashboard from "./pages/admin/Dashboard";
-import AdminLogin from "./pages/admin/Login";
-import Landing from "./pages/Landing";
+import Login from "./pages/admin/Login";
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        {/* Choose Between The Admin App, Student app And The Teacher App */}
-        <Route path="/" element={<Landing />} />
-        {/* Auth  Required */}
-        <Route element={<RouteGuard isAllowed={true} />}>
-          <Route path="admin">
-            <Route index element={<Dashboard />}></Route>
-            <Route path="login" element={<AdminLogin />}></Route>
-          </Route>
+        <Route path="_/login" element={<Login />}></Route>
+        <Route path="/">
+          {/* The School Website */}
         </Route>
+        <Route element={<RouteGuard isAllowed={true} />}>
+          <Route path="_/">
+            <Route index element={<Dashboard />}></Route>
+          </Route>
+          {/* the The Student And teacher dashboard */}
+          <Route element={<RouteGuard isAllowed={false} />}>
+            <Route path="/student">
+                {/* Student dashboard */}
+            </Route>
+            <Route path="/teacher">
+                {/* Teacher dashboard */}
+            </Route>
+              
+          </Route>
+        
+        </Route>
+        <Route path="*" element={<Navigate to="/" replace /> } />
       </Routes>
     </BrowserRouter>
   )
--- Client/src/pages/admin/Login.tsx
+++ Client/src/pages/admin/Login.tsx
@@ -1,27 +1,28 @@
-import { Box, Container, Paper, TextField, Typography, useTheme,Checkbox,FormControlLabel, Button} from '@mui/material'
+import { Box, Container, Paper, TextField, Typography, useTheme,Checkbox,FormControlLabel, Button,colors} from '@mui/material'
 import Logo from "../../assets/img/Savant.svg"
 import "../../styles/login.css"
-import React from 'react'
+import React, { useRef } from 'react'
 
-export default function AdminLogin() {
+export default function Login() {
   const theme = useTheme()
-  
-  
+  const inputName = useRef(null)
+  const inputPassword = useRef(null)
+    
   return (
-    <Box className="outerContainer" sx={{ userSelect:"none", height: "100%", display: "flex" , justifyContent: "center", alignItems: "center" ,bgcolor:"#f3f3f3"}}>
+    <Box sx={{ userSelect:"none", height: "100%", display: "flex" , justifyContent: "center", alignItems: "center" ,bgcolor:colors.grey[100]}}>
       <Container>
         <Box sx={{ display:"flex", justifyContent: "center", alignItems:"center" , gap: theme.spacing(6), flexDirection:"column", width: "100%" }}>
           <img src={Logo} alt="Logo" />
             <Box className='FancyBoxShadow' sx={{ padding: theme.spacing(4), maxWidth: "620px", width: "100%", bgcolor:'white'  }}>
-              <Typography variant="h5" component="h1">Admin Sign In</Typography>
+              <Typography variant="h5" component="h1">Log In</Typography>
                 <form action="" style={{ width:"100%" }}>
                   <Box sx={{ marginTop: theme.spacing(4), display:"flex", justifyContent: "center", alignItems:"center" , gap: theme.spacing(3), flexDirection:"column" }}>
-                      <TextField type={"email"} required label="Email" variant="outlined" fullWidth />
-                      <TextField type={"password"} required label="Password" variant="outlined" fullWidth />
-                      <Box sx={{ width: "100%" }}>
+                      <TextField ref={inputName}  type={"text"} required label="Admin Name" variant="outlined" fullWidth />
+                      <TextField ref={inputPassword} type={"password"} required label="Password" variant="outlined" fullWidth />
+                      {/* <Box sx={{ width: "100%" }}>
                         <FormControlLabel control={<Checkbox />} label="Remember Me" />
-                      </Box>
-                      <Button variant='contained' size='large' type='submit'  fullWidth >SIGN in</Button>
+                      </Box> */}
+                      <Button variant='contained' size='large' type='submit'  fullWidth >login</Button>
 
                   </Box>
                 </form>
